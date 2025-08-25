e08a6f76a90272b90f5ce6caa23658ccc549d8bd
parth <pgpatel018@gmail.com>
2025-08-25 11:18:05 +0530

    test

--- app.js
+++ app.js
@@ -7,11 +7,7 @@ require("dotenv").config();
 const app = express()
 app.use(express.json());
 
-app.use(cors({
-  origin: "*", //has to be changed in production
-  methods: ["GET", "POST", "PUT", "DELETE", "*"],
-  credentials: true
-}));
+app.use(cors());
 
 app.use(
   session({
